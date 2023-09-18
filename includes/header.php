c70c4b530b659acadd0686de531f5198966bb86a
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-09-18 11:39:42 -0700

    diseño Ok

---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<header id="header" class="header-style-1 header-alt">
+    <div class="container">
+        <div class="row clearfix">
+            <div class="header-clear clearfix">
+                <div class="header-content-left clearfix">
+                    <!-- LOGO START
+                                ============================================= -->
+                    <div class="logo">
+                        <a href="index.html"><img src="img/logo_ofrenda.png" alt="Ofrenda by Inferno" /></a>
+                    </div>
+                    <!-- LOGO END -->
+                </div>
+                <div class="header-content-center clearfix">
+                    <!-- NAVIGATION START============================================= -->
+                    <nav class="main-nav">
+                        <!-- Mobile menu toggle button (hamburger/x icon) -->
+                        <input id="main-menu-state" type="checkbox" />
+                        <label class="main-menu-btn sub-menu-trigger" for="main-menu-state">
+                            <span class="main-menu-btn-icon"></span> Toggle main menu visibility
+                        </label>
+                        <!-- Sample menu definition -->
+                        <ul id="main-menu" class="sm sm-clean">
+                            <li class="menu-item"><a href="index.html" class="menu-link">Inicio</a></li>
+                            <li class="menu-item"><a href="about.html" class="menu-link">Nosotros</a></li>
+                            <li class="menu-item"><a href="event.html" class="menu-link">Comprar Tickets</a></li>
+                            <li class="menu-item"><a href="contact.html" class="menu-link">Contacto</a></li>
+                        </ul>
+                    </nav>
+                    <!-- NAVIGATION END -->
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<?php
+    include("includes/head.php");
+?>
+<body class="demo-1">
+       <div class="main-wrapper clearfix">
+       <?php
+            include("includes/header.php");
+            include("modules/index.php");
+            include("includes/redes.php");
+            include("includes/footer.php");
+        ?>
+       </div>
+        <?
+            include("includes/scripts.php");
+        ?>
+</body>
+
+</html>
