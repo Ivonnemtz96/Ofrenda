c2b98b18f9d93e1df0732c70593e70485e28d553
Ivonne Mtz <54880593+Ivonnemtz96@users.noreply.github.com>
2023-09-22 14:24:18 -0700

    footer ok

--- includes/footer.php
+++ includes/footer.php
@@ -1,58 +1,15 @@
  <!-- Footer -->
- <footer class="main-footer dark">
-     <div class="container">
-         <div class="row">
-             <div class="col-md-3 mb-30">
-                 <div class="item abot">
-                     <div class="logo mb-20">
-                         <h2><a href="index.html">winta</a><span>Steakhouse+Bar</span></h2>
-                     </div>
-                     <p>Steakhouse the nunc bibe endum in finibus elit the solli citudin elit.</p>
-                     <div class="social-icon">
-                         <a href="index.html"><i class="ti-facebook"></i></a>
-                         <a href="index.html"><i class="ti-twitter"></i></a>
-                         <a href="index.html"><i class="ti-instagram"></i></a>
-                         <a href="index.html"><i class="ti-pinterest"></i></a>
-                     </div>
-                 </div>
-             </div>
-             <div class="col-md-2 offset-md-1 mb-30">
-                 <div class="item fotcont">
-                     <div class="fothead">
-                         <h6>Phone & Email</h6>
-                     </div>
-                     <p>+1 203-123-0606</p>
-                     <p>steakhouse@winta.com</p>
-                 </div>
-             </div>
-             <div class="col-md-2 offset-md-1 mb-30">
-                 <div class="item fotcont">
-                     <div class="fothead">
-                         <h6>Our Address</h6>
-                     </div>
-                     <p>24 King St, Charleston, SC 29401 USA</p>
-                 </div>
-             </div>
-             <div class="col-md-2 offset-md-1 mb-30">
-                 <div class="item fotcont">
-                     <div class="fothead">
-                         <h6>Opening Hours</h6>
-                     </div>
-                     <p>Mon-Fri: 5 PM - 10:30 PM Sunday: Closed</p>
-                 </div>
-             </div>
-         </div>
-     </div>
+ <footer class="main-footer dark" style="padding: 0;">
      <div class="sub-footer">
          <div class="container">
              <div class="row">
                  <div class="col-md-8">
                      <div class="text-left">
-                         <p>© 2022, Winta Template. All right reserved.</p>
+                         <p>© 2023, Ofrenda by Inferno Fest. Derechos reservados.</p>
                      </div>
                  </div>
                  <div class="col-md-4">
-                     <p class="right"><a href="#">Terms & Conditions</a></p>
+                     <p class="right">Desarrollado por <a target="_blank" href="bananagroup.mx/"><strong> Banana Group Marketing</strong></a></p>
                  </div>
              </div>
          </div>
